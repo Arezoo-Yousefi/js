c5ccd6d347afab879b607851ac62e06bf563e1f9
Arezoo Yousefi <are_yousefi@yahoo.com>
2021-02-02 19:42:27 -0800

    ex29

---
+++ index29.js
@@ -0,0 +1,5 @@
+const length = 10;
+let perimeter = "length*4";
+console.log("in a square the perimeter is ",perimeter);
+perimeter = length * 4;
+console.log("if length is 10 so perimeter = ",perimeter);
