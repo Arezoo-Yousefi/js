075f5d9b0e199784145a49d93b7e28dbbdea303c
Arezoo Yousefi <are_yousefi@yahoo.com>
2021-02-02 19:30:42 -0800

    ex27-28

---
+++ index27.js
@@ -0,0 +1,12 @@
+let  height = 165 ;
+let  weight = 63 ;
+let result1 = (height + weight);
+let result2 = (height - weight);
+let result3 = (height * weight);
+let result4 = (height / weight);
+let result5 = (height % weight);
+console.log("sum = ",result1);
+console.log("subtraction = ",result2);
+console.log("multipication = ",result3);
+console.log("division = ",result4);
+console.log("remainder = ",result5);
