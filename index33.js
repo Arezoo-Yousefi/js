7005ad6b862040be1b299fde18d340a6d3228d51
Arezoo Yousefi <are_yousefi@yahoo.com>
2021-02-03 17:53:37 -0800

    ex33

---
+++ index33.js
@@ -0,0 +1,8 @@
+let userCount = 100;
+userCount += 5;
+console.log("Users count: ", userCount);
+userCount *= 2;
+console.log("Users count: ", userCount);
+let men = userCount / 2;
+let women = userCount - men;
+console.log("Users count:", userCount + ",  men:", men + ",  women:", women );
