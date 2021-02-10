4e2cf9bc84350b33cd873cc0a080cf9963505c7a
Arezoo Yousefi <are_yousefi@yahoo.com>
2021-02-09 22:18:05 -0800

    ex56-7

---
+++ index57.js
@@ -0,0 +1,4 @@
+let username = "pepe2017";
+const password = "12345";
+let message = (username == "pepe2017"&& password =="12345") ? "Logged in user, show user home page, \nyour name" : "sorry there is aproblem please try again";
+console.log(message); 
