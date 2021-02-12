6523303f66f495f14adda777d9793a16304298ec
Arezoo Yousefi <are_yousefi@yahoo.com>
2021-02-12 15:55:00 -0800

    ex117-120

---
+++ index117.js
@@ -0,0 +1,2 @@
+const studens = ["Parmida", "Mantra", "Mahtab", "Jalleh", "hana"];
+console.log(`first : ${studens[0]}\nthird : ${studens[2]}\nfifth : ${studens[4]}`);
---
+++ index120.js
@@ -0,0 +1,13 @@
+const students = [];
+students[0] = "Arezoo";
+students[1] = "Parmida";
+students[2] = "Mahtab";
+students[3] = "Ali";
+students[4] = "Hana";
+students[5] = "Georg";
+function name(n){
+    console.log(`index ${n}: ${students[n]}`);
+}
+for (let i = 0; i <=5; i++){
+    name(i);
+} 
